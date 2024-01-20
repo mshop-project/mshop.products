330d4a96a036574fee171d64e95df71550629084
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2024-01-20 15:51:40 +0100

    feat: create entities

---
+++ mshop.products.domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace mshop.products.domain.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ mshop.products.domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace mshop.products.domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+
+        public Category Category { get; set; } = null!;
+        public Guid CategoryId { get; set; }
+    }
+}
