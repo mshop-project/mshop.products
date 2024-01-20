a613ded07075444c2c4a48756853a8448374c3b9
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2024-01-20 15:26:04 +0100

    feat: create clean architecture

---
+++ mshop.products.application/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace mshop.products.application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ mshop.products.infrastructure/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace mshop.products.infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
